7ac14dc8ae4cfe9a943d4c6a48322298dcfbf402
emmup <40918806+emmup@users.noreply.github.com>
2025-04-30 02:40:28 +0700

    Update translate.js

--- api/translate.js
+++ api/translate.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   const { text } = req.query;
 
   if (!text) {
@@ -10,10 +19,6 @@ export default async function handler(req, res) {
     const apiUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=id&dt=t&q=${encodeURIComponent(text)}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
-
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     res.status(200).json(data);
   } catch (error) {
